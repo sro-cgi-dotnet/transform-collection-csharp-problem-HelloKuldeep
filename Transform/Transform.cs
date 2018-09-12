857d804cb4c163c9c4e4284f27ecbb16bafa196b
Kuldeep Sharma <kul123sharma@gmail.com>
2018-09-12 14:36:37 +0530

    Transform-Solution-@hellokuldeep-1.1

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -2,13 +2,14 @@
 using System.Linq;
 using System.Collections.Generic;
 
-namespace Transform
-{
-    public static class Transform 
-    {
-        public static void Map()
-        {
-            throw new NotImplementedException();
+namespace Transform{
+    public delegate int Ddouble(int x);
+    public static class Transform {
+        public static int[] Map(this int[] a,Ddouble synt){
+            for (int i = 0; i < a.Length; i++){
+                a[i]=synt(a[i]);
+            }
+            return a;
         }
     }
 }
